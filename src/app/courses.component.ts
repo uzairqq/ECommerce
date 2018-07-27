cded07db791ab2d38f220d5a1e039390edc38ef8
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-27 17:36:58 +0500

    First Working Component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'courses',
-    template: '<h1>Courses</h1>',
+    template: '<h1>Courses</h1>'
 
 })
 
