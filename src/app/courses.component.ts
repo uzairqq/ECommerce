832a0a224aa02e1710518812d3f60ea22bba71cc
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-27 20:25:45 +0500

    added bootstrap in angular application

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,15 +5,11 @@ import { CoursesService } from './couses.service';
     // tslint:disable-next-line:component-selector
     selector: 'courses',
     template: `
-    <h1>{{title}}</h1>
-    <img src={{imageUrl}}/>
-    <img [src]="imageUrl"/>
+  <button class="btn btn-primary">Save</button>
+
     `
 
 })
 
 export class CoursesComponent {
-    title = 'List Of Course';
-    imageUrl = 'http://bookmarkurl.info/images/place/place-1.jpg';
-
 }
