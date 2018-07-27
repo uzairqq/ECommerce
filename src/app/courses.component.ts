d1895db6df5081c5dae5c9defc54bcf8275c33e1
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-27 18:22:31 +0500

    used First Directive *ngFor

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'courses',
-    template: '<h1>{{title}}</h1>'
+    template: `<h1>
+    {{title}}</h1>
+    <ul>
+    <li *ngFor="let course of courses">
+    {{course}}
+    </li>
+    </ul>
+    `
 
 })
 
 export class CoursesComponent {
     title = 'List Of Course';
+    courses = ['english', 'urdu', 'maths'];
 }
