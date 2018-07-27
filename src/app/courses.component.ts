b5b6c44f26b1865e5f6ae32833e752079cd0cd24
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-27 17:29:47 +0500

    basic course component

---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'courses',
+    template: '<h1>Courses</h1>',
+
+})
+
+export class CoursesComponent {
+    // tslint:disable-next-line:eofline
+}
