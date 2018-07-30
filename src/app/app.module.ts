db266ea7c42c671cfd2d0c528cdd5320526f702f
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-31 01:18:46 +0500

    Created and used Custom Pipes for text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './couses.service';
 import { FormsModule } from '@angular/forms';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,21 +5,14 @@ import { CoursesService } from './couses.service';
     // tslint:disable-next-line:component-selector
     selector: 'courses',
     template: `
-    {{course.title | uppercase | lowercase}}<br/>
-    {{course.students | number }}<br/>
-    {{course.rating | number:'1.2-2'}} <br/>
-    {{course.price | currency:'$':true:'3.2-2' }} <br/>
+    {{text | summary:10}}
     `
 
 })
 
 export class CoursesComponent {
 
-    course = {
-        title: 'The Angular Couse',
-        rating: 4.975,
-        students: 30123,
-        price: 190.95,
-    };
+    // tslint:disable-next-line:max-line-length
+    text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
 
 }
