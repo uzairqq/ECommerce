c9f2c8de2b2c5a7c2755f9191ffd9dd860f8188f
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-01 22:26:31 +0500

    used ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +9,26 @@ import { Component } from '@angular/core';
   <p>Value from child Component is </p>
   Message: {{message}}
   <app-child (messageEvent)="receiveMessage($event)" ></app-child>
- 
+  <input type="text" #myInputText>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('myInputText') inputText;
+
+  constructor() {
+    setInterval(() => { this.send2Server(); }, 2000);
+  }
+
   message = 'hello world parent here';
   receiveMessage($event) {
     this.message = $event;
   }
+
+  send2Server() {
+    const data = this.inputText.nativeElement;
+    console.log(data.Value);
+  }
+
 }
