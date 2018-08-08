7eb7b18d3512be17770700e2ee134f12393b6f01
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-08 16:35:53 +0500

    used *ngFor And Change Detection in ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,12 @@ import { Component, ViewChild } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
+  <button (click)="onAdd()">Add</button>
 <ul>
 <li *ngFor="let course of courses;index as i">
 {{i}}--{{course.name}}
+<button (click)="onChange(course)">Change</button>
+<button (click)="onRemove(course)">Remove</button>
 </li>
 </ul>
   `,
@@ -19,7 +22,16 @@ export class AppComponent {
     { id: 2, name: 'course2' },
     { id: 3, name: 'course3' },
   ];
-
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' });
+  }
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onChange(course) {
+    course.name = 'Updated';
+  }
 
 
 
