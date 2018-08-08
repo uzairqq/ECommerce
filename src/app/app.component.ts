3841815c683311294a5ec474e091f9409efe17db
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-08 13:19:49 +0500

    Used Hidden feild for div hiding .remember use ngif for large tree elements and hidden feild for small tree elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,18 @@ import { Component, ViewChild } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-  <div *ngIf="courses.length>0; then courseList else noCourses"></div>
-  <ng-template #courseList>
-  List Of Courses
-  </ng-template>
-  <ng-template #noCourses>
-  No Courses Yet
-  </ng-template>
+<div [hidden]="courses.length==0">
+List  of Courses
+</div>
+<div [hidden]="courses.length>0">
+No Course Yet
+</div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  courses = [1, 2];
+  courses = [];
 
 
 
