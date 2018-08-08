f7b58f81d2fb90826234fb10d7dcba299d0bf30e
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-08 14:58:26 +0500

    Used NgSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,21 @@ import { Component, ViewChild } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-<div [hidden]="courses.length==0">
-List  of Courses
-</div>
-<div [hidden]="courses.length>0">
-No Course Yet
+<ul class="nav nav-pills">
+<li [class.active]="viewMode =='map' "><a (click)="viewMode ='map' ">Map View</a></li>
+<li [class.active]="viewMode=='list' "><a (click)="viewMode ='list'">List View</a></li>
+</ul>
+<div [ngSwitch]="viewMode">
+<div *ngSwitchCase="'map'">Map View Content</div>
+<div *ngSwitchCase="'list'">List View Content</div>
+<div *ngSwitchDefault>OtherWise</div>
 </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  courses = [];
-
+  viewMode = 'map';
 
 
 
