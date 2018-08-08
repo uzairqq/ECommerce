5382cba2b012374b9326810d37742934f1482b78
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-08 16:25:36 +0500

    used *ngFor Directive for iterate the list and show indexes of each like new feature in for looop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,21 @@ import { Component, ViewChild } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-<ul class="nav nav-pills">
-<li [class.active]="viewMode =='map' "><a (click)="viewMode ='map' ">Map View</a></li>
-<li [class.active]="viewMode=='list' "><a (click)="viewMode ='list'">List View</a></li>
+<ul>
+<li *ngFor="let course of courses;index as i">
+{{i}}--{{course.name}}
+</li>
 </ul>
-<div [ngSwitch]="viewMode">
-<div *ngSwitchCase="'map'">Map View Content</div>
-<div *ngSwitchCase="'list'">List View Content</div>
-<div *ngSwitchDefault>OtherWise</div>
-</div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  viewMode = 'map';
-
+  courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+  ];
 
 
 
