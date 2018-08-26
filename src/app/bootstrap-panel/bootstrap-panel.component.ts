b3921e811bbf36b45346279a62a931550c5f806c
Uzair Iqbal <uzair.qq@outlook.com>
2018-08-27 02:46:15 +0500

    created new form in bootstrap-panel for validation in template driven forms

--- src/app/bootstrap-panel/bootstrap-panel.component.ts
+++ src/app/bootstrap-panel/bootstrap-panel.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bootstrap-panel',
   templateUrl: './bootstrap-panel.component.html',
   styleUrls: ['./bootstrap-panel.component.css']
 })
-export class BootstrapPanelComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class BootstrapPanelComponent {
 
 }
